bf885b4a7c3ca73eaac88c0b9d3ab07ff96167b1
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-12-19 07:53:27 +0400

    Add interactive chip

--- app/src/main/java/dev/hloth/medsreminder/tile/MainTileService.kt
+++ app/src/main/java/dev/hloth/medsreminder/tile/MainTileService.kt
@@ -1,12 +1,20 @@
 package dev.hloth.medsreminder.tile
 
 import android.content.Context
+import androidx.wear.protolayout.ActionBuilders
 import androidx.wear.protolayout.ColorBuilders.argb
+import androidx.wear.protolayout.DimensionBuilders.expand
 import androidx.wear.protolayout.LayoutElementBuilders
+import androidx.wear.protolayout.ModifiersBuilders
+import androidx.wear.protolayout.ModifiersBuilders.Clickable
 import androidx.wear.protolayout.ResourceBuilders
 import androidx.wear.protolayout.TimelineBuilders
 import androidx.wear.protolayout.material.Colors
 import androidx.wear.protolayout.material.Text
+import androidx.wear.protolayout.material.ChipColors
+import androidx.wear.protolayout.material.Button
+import androidx.wear.protolayout.material.ButtonColors
+import androidx.wear.protolayout.material.Chip
 import androidx.wear.protolayout.material.Typography
 import androidx.wear.protolayout.material.layouts.PrimaryLayout
 import androidx.wear.tiles.RequestBuilders
@@ -15,13 +23,11 @@ import androidx.wear.tiles.tooling.preview.Preview
 import androidx.wear.tiles.tooling.preview.TilePreviewData
 import androidx.wear.tooling.preview.devices.WearDevices
 import com.google.android.horologist.annotations.ExperimentalHorologistApi
+import com.google.android.horologist.compose.tools.buildDeviceParameters
 import com.google.android.horologist.tiles.SuspendingTileService
 
 private const val RESOURCES_VERSION = "0"
 
-/**
- * Skeleton for a tile with no images.
- */
 @OptIn(ExperimentalHorologistApi::class)
 class MainTileService : SuspendingTileService() {
 
@@ -71,14 +77,23 @@ private fun tileLayout(
     return PrimaryLayout.Builder(requestParams.deviceConfiguration)
         .setResponsiveContentInsetEnabled(true)
         .setContent(
-            Text.Builder(context, "Hello World!")
-                .setColor(argb(Colors.DEFAULT.onSurface))
-                .setTypography(Typography.TYPOGRAPHY_CAPTION1)
-                .build()
-        ).build()
+            Chip.Builder(
+                context,
+                Clickable.Builder()
+                    .setId("med_1")
+                    .setOnClick(ActionBuilders.LoadAction.Builder().build())
+                    .build(),
+                buildDeviceParameters(context.resources))
+                    .setPrimaryLabelContent("hello")
+//                    .setSecondaryLabelContent(contentChipSecondaryLabelText)
+//                    .setIconContent(container.container)
+                    .setWidth(expand())
+                .setChipColors(ChipColors.secondaryChipColors(Colors.DEFAULT))
+                    .build()
+        )
+        .build()
 }
 
-@Preview(device = WearDevices.SMALL_ROUND)
 @Preview(device = WearDevices.LARGE_ROUND)
 fun tilePreview(context: Context) = TilePreviewData(::resources) {
     tile(it, context)
